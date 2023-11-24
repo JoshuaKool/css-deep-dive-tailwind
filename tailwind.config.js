3d3a711a121cd412a56f68c587e37dbee1e91385
Joshua Kool <177300@student.horizoncollege.nl>
2023-11-24 11:50:31 +0100

    update

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      'black': '#020617'
+    },
+    container: {
+      center: true
+    },
+    extend: {},
+  },
+  plugins: [],
+}
